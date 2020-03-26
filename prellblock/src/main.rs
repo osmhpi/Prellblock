16a907e296e5c732bf76562fe7a3b5c87af0e997
Malte Andersch <malte.andersch@student.hpi.de>
2020-03-26 14:31:21 +0100

    Add pretty_env_logger to the project

--- prellblock/src/main.rs
+++ prellblock/src/main.rs
@@ -7,10 +7,24 @@
 //! By using an execute-order-validate procedure it is assured, that data will be saved, even in case of a total failure of all but one redundant processing unit.
 //! While working in full capactiy, data is stored and validated under byzantine fault tolerance. This project is carried out in cooperation with **Deutsche Bahn AG**.
 
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+mod util;
+use util::some_test;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+    trace!("leave no trace");
+    debug!("debooging");
+    info!("such information");
+    warn!("o_O");
+    error!("much error");
+    some_test();
 }
 
-pub fn Foo() {
+/// This is the **FOO FIGHTER**
+pub fn foo() {
     println!("Malte says hi");
 }
---
+++ prellblock/src/util.rs
@@ -0,0 +1,7 @@
+pub(crate) fn some_test() {
+    trace!("leave no trace");
+    debug!("debooging");
+    info!("such information");
+    warn!("o_O");
+    error!("much error");
+}
