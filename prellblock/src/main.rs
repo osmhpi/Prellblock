d89857876ec754d657e8c340251de8f73b1e1878
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2020-03-26 11:48:13 +0100

    Setup the CI pipeline

--- prellblock/src/main.rs
+++ prellblock/src/main.rs
@@ -1,4 +1,4 @@
-#![warn(missing_docs)]
+#![warn(missing_docs, clippy::all, clippy::pedantic, clippy::nursery)]
 //! Bahndaten verlässlich und schnell in die Blockchain gepuffert - **Persistente Redundante Einheit für Langzeit-Logging über Blockchain**
 //!
 //! ## Overview
@@ -7,8 +7,10 @@
 //! By using an execute-order-validate procedure it is assured, that data will be saved, even in case of a total failure of all but one redundant processing unit.
 //! While working in full capactiy, data is stored and validated under byzantine fault tolerance. This project is carried out in cooperation with **Deutsche Bahn AG**.
 
-
-
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn Foo() {
+    println!("Malte says hi");
 }
