284763be585bbfd260a375aee6be7fbea6df3c72
Felix Gohla <felix.gohla@student.hpi.de>
2020-05-06 15:56:32 +0200

    Only read CA certificate once
    - Before, the CA certificate was read on every connection

--- balise/src/client/stream_impl_tls.rs
+++ balise/src/client/stream_impl_tls.rs
@@ -1,7 +1,8 @@
 use super::BoxError;
+use lazy_static::lazy_static;
 use native_tls::{Certificate, TlsConnector};
-use std::net::SocketAddr;
-use tokio::{fs, net::TcpStream};
+use std::{fs, net::SocketAddr};
+use tokio::net::TcpStream;
 use tokio_tls::{TlsConnector as AsyncTlsConnector, TlsStream};
 
 pub type StreamImpl = TlsStream<TcpStream>;
@@ -12,18 +13,22 @@ impl<'a> super::StreamGuard<'a> {
     }
 }
 
-pub async fn connect(addr: &SocketAddr) -> Result<StreamImpl, BoxError> {
-    // open certificate file
-    let buffer = fs::read("./certificates/ca/ca_prellblock-ca.cert").await?;
-    //load certificate from file
-    let cert = Certificate::from_pem(&buffer)?;
-    // new builder with trusted root cert
-    let mut builder = TlsConnector::builder();
-    builder.add_root_certificate(cert);
-    let connector: AsyncTlsConnector = builder.build()?.into();
+lazy_static! {
+    static ref CONNECTOR: AsyncTlsConnector = {
+        // open certificate file
+        let buffer = fs::read("./certificates/ca/ca_prellblock-ca.cert").unwrap();
+        //load certificate from file
+        let cert = Certificate::from_pem(&buffer).unwrap();
+        // new builder with trusted root cert
+        let mut builder = TlsConnector::builder();
+        builder.add_root_certificate(cert);
+        builder.build().unwrap().into()
+    };
+}
 
+pub async fn connect(addr: &SocketAddr) -> Result<StreamImpl, BoxError> {
     // connect with tcp stream
     let stream = TcpStream::connect(addr).await?;
-    let stream = connector.connect(&addr.ip().to_string(), stream).await?;
+    let stream = CONNECTOR.connect(&addr.ip().to_string(), stream).await?;
     Ok(stream)
 }
