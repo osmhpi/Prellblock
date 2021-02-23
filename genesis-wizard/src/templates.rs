f708e999b8f4a24b84e3af02d25bc6eb4ba705f7
Robert Schmid <r.schmid@outlook.com>
2021-02-23 16:35:59 +0000

    Remove dead code

--- genesis-wizard/src/templates.rs
+++ genesis-wizard/src/templates.rs
@@ -3,17 +3,7 @@ use prellblock_client_api::account::Permissions;
 use serde::{Deserialize};
 
 #[derive(Deserialize)]
-pub struct AccountTypeTemplate {
-    #[serde(rename = "type")]
-    pub account_type: String,
-    pub turi_address: String,
-    pub peer_address: String
-}
-
-#[derive(Deserialize)]
 pub struct AccountTemplate {
     pub name: String,
-    // #[serde(rename = "type")]
-    // pub account_type: AccountTypeTemplate
     pub permissions: Permissions
 }
